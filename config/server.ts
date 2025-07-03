37608021dd5cdb2db0b7c1f44e1910655f5a49e5
giovanni <giovanni@MacBook-Air-di-giovanni.local>
2025-07-03 22:28:39 +0200

    added pg package2

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,11 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+  logger: {
+    level: "debug", // 👈 oppure 'info' o 'trace' per log più dettagliati
+    requests: true,
   },
 });
